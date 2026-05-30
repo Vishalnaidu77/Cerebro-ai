7447a5ba76475f7df9125b68673e04d0a1fec89d
vishalnaidu77 <vminorr@gmail.com>
2026-05-30 08:30:42 +0530

    feat: implement user authentication flow including login and registration pages, Redux state management, and API services using 4 layer or React architecture

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { getMe, login, register } from '../services/auth.api'
+import { useDispatch } from 'react-redux'
+import { setError, setLoading, setUser } from '../auth.slice'
+
+const useAuth = () => {
+
+    const dispatch = useDispatch()
+
+    const handleRegister = async (username, email, password) => {
+        try {
+            dispatch(setLoading(true))
+            const res = await register(username, email, password)
+            dispatch(setUser(res.user))
+        } catch (err) {
+            dispatch(setError(err.message))
+        } finally{
+            dispatch(setLoading(false))
+        }
+    }
+
+    const handleLogin = async (email, password) => {
+       try {
+        dispatch(setLoading(true))
+        const res = await login(email, password)
+        dispatch(setUser(res.user))
+       } catch (err) {
+        dispatch(setError(err.message))
+       } finally{
+        dispatch(setLoading(false))
+       }
+    }
+
+    const handleGetMe = async () => {
+        try {
+            dispatch(setLoading(true))
+            const res = await getMe()
+            dispatch(setUser(res.user))
+        } catch (err) {
+            dispatch(setError(err.message))
+        } finally{
+            dispatch(setLoading(false))
+        }
+    }
+
+  return {
+    handleRegister,
+    handleLogin,
+    handleGetMe
+  }
+}
+
+export default useAuth
---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: "http://localhost:8000/api/auth",
+    withCredentials: true
+})
+
+export async function register(username, email, password) {
+    const res = await api.post("/register", {
+        username,
+        email, 
+        password
+    })
+
+    return res.data
+}
+
+export async function login(email, password) {
+    const res = await api.post("/login", {
+        email,
+        password
+    })
+
+    return res.data
+}
+
+export async function getMe() {
+    const res = await api.post("/getme")
+    return res.data
+}
